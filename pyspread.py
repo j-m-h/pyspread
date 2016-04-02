afb77913ef3fc3b41f330f74ee828687980cc4b4
j-m-h <james.m.hulett@sbcglobal.net>
2016-04-01 21:29:51 -0700

    Fully implemented _call_script

--- pyspread.py
+++ pyspread.py
@@ -1,12 +1,16 @@
 from apiclient import errors
 
-class scriptError(Exception):
+class scriptCallError(Exception):
 	"""An error caused by the script failing to run."""
+	pass # everything can be inherited from Exception
+class scriptRuntimeError(Exception):
+	"""An error raised by the script during execution."""
 	pass # everything can be inherited from Exception
 
 
 def _call_script(service, function_name, params):
 	"""Calls a given function from the background google apps script.
+	Adapted from the google tutorial on calling scripts: https://developers.google.com/apps-script/guides/rest/quickstart/python#step_3_set_up_the_sample
 
 	Params:
 		service: the object returned by a call of the form discovery.build('script', 'v1', http=http)
@@ -17,38 +21,35 @@ def _call_script(service, function_name, params):
 	Returns:
 		Whatever the google apps script returns if the run is successful
 
+	Possible errors:
+		Raises a scriptCallError if the script errors before running
+		Raises a scriptRuntimeError if the script errors while running
 
+	Note that this function should only be called internally.
 	"""
-	# Create an execution request object.
-	request = {"function": "getUserFromID", "parameters": [str(id_num)]}
+	# Create an execution request.
+	request = {"function": function_name, "parameters": params, "devMode": True}
 	# NOTE: Turn OFF devMode once this is out of the testing phase
 
 	try:
 		# Make the API request.
-		response = service.scripts().run(body=request,
-			scriptId=SCRIPT_ID).execute()
+		response = service.scripts().run(body=request, scriptId=SCRIPT_ID).execute()
 
 		if 'error' in response:
 			# The API executed, but the script returned an error.
-
-			# Extract the first (and only) set of error details. The values of
-			# this object are the script's 'errorMessage' and 'errorType', and
-			# an list of stack trace elements.
 			error = response['error']['details'][0]
-			print("Script error message: {0}".format(error['errorMessage']))
+			error_message = error['errorMessage']
 
 			if 'scriptStackTraceElements' in error:
-				# There may not be a stacktrace if the script didn't start
-				# executing.
-				print("Script error stacktrace:")
+				# There may not be a stacktrace if the script didn't start executing.
+				error_message += "\nStacktrace:"
 				for trace in error['scriptStackTraceElements']:
-					print("\t{0}: {1}".format(trace['function'],
-					    trace['lineNumber']))
-			return "" # if an error occured, can't get the name back
+					error_message += "\n\t{0}: {1}".format(trace['function'], trace['lineNumber'])
+			raise scriptRuntimeError("Error while calling function " + function_name + " with parameters " + params + "\nError message: " + error_message)
 		else:
-			# means the request went through without error, so return what the request returned --> a name or the empty string
-			return str(response['response']['result'])
+			# means the request went through without error, so return what the request returned
+			return response['response']['result']
 
 	except errors.HttpError as e:
 		# The API encountered a problem before the script started executing.
-		print(e.content)
+		raise scriptCallError("Failed to call function " + function_name + " with parameters " + params + "\nError message: " + e.content)
